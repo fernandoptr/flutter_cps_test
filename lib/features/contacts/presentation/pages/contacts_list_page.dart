38f5526e905432e42acc8e0f8b57fc10852f2a65
Fernando Putra <fernnandoptr@gmail.com>
2024-03-29 14:50:00 +0700

    refactor(widgets): Replace contacts list extracted widgets into private widgets
    - Extracted contacts list related widgets into private widgets within the same file.
    - Adjusted project file imports.

--- lib/features/contacts/presentation/pages/contacts_list_page.dart
+++ lib/features/contacts/presentation/pages/contacts_list_page.dart
@@ -1,5 +1,8 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import '../../../../core/extensions/extensions.dart';
+import '../../domain/entities/entities.dart';
 
 import '../blocs/blocs.dart';
 import '../widgets/widgets.dart';
@@ -16,7 +19,7 @@ class ContactsListPage extends StatelessWidget {
         },
         child: CustomScrollView(
           slivers: [
-            _buildAppBar(),
+            _ContactsListAppBar(),
             _buildBody(),
           ],
         ),
@@ -24,31 +27,89 @@ class ContactsListPage extends StatelessWidget {
     );
   }
 
-  ContactsListAppBar _buildAppBar() {
-    return const ContactsListAppBar();
-  }
-
   BlocBuilder<ContactsListBloc, ContactsListState> _buildBody() {
     return BlocBuilder<ContactsListBloc, ContactsListState>(
       builder: (context, state) {
         return switch (state.status) {
-          ContactsListStatus.initial ||
-          ContactsListStatus.loading =>
+          (ContactsListStatus.initial || ContactsListStatus.loading) =>
             const SliverFillRemaining(
-              child: Center(
-                child: CircularProgressIndicator(),
-              ),
+              child: Center(child: CircularProgressIndicator()),
             ),
-          ContactsListStatus.success => ContactsListScrollView(
+          (ContactsListStatus.success) => _ContactsListScrollView(
               contacts: state.contacts,
             ),
-          ContactsListStatus.failure => SliverFillRemaining(
-              child: Center(
-                child: Text(state.errorMessage),
-              ),
+          (ContactsListStatus.failure) => SliverFillRemaining(
+              child: Center(child: Text(state.errorMessage)),
             ),
         };
       },
+    );
+  }
+}
+
+class _ContactsListAppBar extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return SliverAppBar.medium(
+      title: const Text('Contacts'),
+      titleTextStyle: context.textTheme.displaySmall?.copyWith(
+        fontWeight: FontWeight.bold,
+      ),
+      pinned: true,
+      stretch: true,
+      actions: [
+        BlocBuilder<ContactsListBloc, ContactsListState>(
+          builder: (context, state) {
+            return SortByNameToggler(
+                isSortAscByName: state.isSortAscByName,
+                onSortByNameToggled: () => context
+                    .read<ContactsListBloc>()
+                    .add(ContactsListSortByNameToggled()));
+          },
+        )
+      ],
+      bottom: PreferredSize(
+        preferredSize: const Size.fromHeight(kToolbarHeight),
+        child: Padding(
+          padding: const EdgeInsets.symmetric(
+            horizontal: 16.0,
+            vertical: 8.0,
+          ),
+          child: CupertinoSearchTextField(
+            autocorrect: false,
+            keyboardType: TextInputType.name,
+            placeholder: 'Search contacts',
+            onChanged: (value) => context
+                .read<ContactsListBloc>()
+                .add(ContactsListSearchTextChanged(value)),
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class _ContactsListScrollView extends StatelessWidget {
+  final List<Contact> contacts;
+
+  const _ContactsListScrollView({required this.contacts});
+
+  @override
+  Widget build(BuildContext context) {
+    return SliverList(
+      delegate: SliverChildBuilderDelegate(
+        (BuildContext context, int index) {
+          final contact = contacts[index];
+          return ListTile(
+            title: Text(contact.name),
+            subtitle: Text('${contact.phoneNumber} • ${contact.city}'),
+            leading: CircleAvatar(
+              child: Text(contact.name[0].toUpperCase()),
+            ),
+          );
+        },
+        childCount: contacts.length,
+      ),
     );
   }
 }
--- lib/features/contacts/presentation/widgets/widgets.dart
+++ lib/features/contacts/presentation/widgets/widgets.dart
@@ -1,3 +1,1 @@
-export 'contacts_list_app_bar.dart';
-export 'contacts_list_scroll_view.dart';
 export 'sort_by_name_toggler.dart';
