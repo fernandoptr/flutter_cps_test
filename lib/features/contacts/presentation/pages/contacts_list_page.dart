bd51f0d9726bbbae5ffa96ddd6393ada683d2af0
Fernando Putra <fernnandoptr@gmail.com>
2024-03-29 14:24:45 +0700

    fix(widgets): Add missing export for ContactsListScrollView
    - Add export 'contacts_list_scroll_view.dart' to the widgets.dart file.

--- lib/features/contacts/presentation/pages/contacts_list_page.dart
+++ lib/features/contacts/presentation/pages/contacts_list_page.dart
@@ -2,7 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
 import '../blocs/blocs.dart';
-import '../widgets/contacts_list_sliver_list.dart';
 import '../widgets/widgets.dart';
 
 class ContactsListPage extends StatelessWidget {
--- lib/features/contacts/presentation/widgets/widgets.dart
+++ lib/features/contacts/presentation/widgets/widgets.dart
@@ -1,1 +1,2 @@
 export 'contacts_list_app_bar.dart';
+export 'contacts_list_scroll_view.dart';
